c7967faec6ad1a3f3c940ce698f52e5825ef2079
Val435 <ia.valentina@ufg.edu.sv>
2024-08-16 21:48:43 -0600

    codefirst

---
+++ database/migrations/2024_08_15_222549_create_notas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('estudiante_id')->constrained()->onDelete('cascade');
+            $table->foreignId('materia_id')->constrained()->onDelete('cascade');
+            $table->decimal('nota', 5, 2); 
+            $table->timestamps();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notas');
+    }
+};
